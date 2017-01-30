5122d5c099648d002d934f3cc5cddc9ec603a7c1
Luis Rovirosa <luisrovirosa@gmail.com>
2017-01-30 17:50:34 +0100

    Acceptance tests names

---
+++ src/test/java/SubmitPaymentShould.java
@@ -0,0 +1,12 @@
+public class SubmitPaymentShould {
+
+    // ensure_the_shopping_basket_has_items
+    // check_that_all_the_items_are_in_stock
+    // abort_payment_when_there_is_an_item_out_of_stock
+    // make_the_payment_against_the_payment_gateway
+    // send_an_email_when_the_payment_is_successful
+
+    // returns_ok_when_everything_went_well
+    // returns_fail_with_the_message_when_there_is_a_problem
+
+}
